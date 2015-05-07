be3640bbdaa9a720491a72549471dbcd0e945e6f
KiwiBA <kiwi.bu@web.de>
2015-05-07 11:49:32 +0200

    DB modell komplett eingefügt

--- StudentenKochen/recipes/admin.py
+++ StudentenKochen/recipes/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
+from .models import Person
+from .models import Tag
 from recipes.models import Ingredient
 from .models import Recipe
 from .models import Recipeingredients
+from .models import Comment
+from .models import Rating
 
 
+admin.site.register(Person)
+admin.site.register(Tag)
 admin.site.register(Ingredient)
 admin.site.register(Recipe)
 admin.site.register(Recipeingredients)
+admin.site.register(Comment)
+admin.site.register(Rating)
--- StudentenKochen/recipes/models.py
+++ StudentenKochen/recipes/models.py
@@ -3,6 +3,15 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+class Person(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=254, null=True, unique=True)
+    username = models.CharField(max_length=200)
+    pw = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.email + " --- " + self.username
+
 class Ingredient(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
@@ -10,12 +19,21 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
     
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    tagname = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.tagname
+    
 class Recipe(models.Model):
     id = models.AutoField(primary_key=True)
     recipename = models.CharField(max_length=200)
+    author = models.ForeignKey(Person, null=True)
     pub_date = models.DateTimeField('date published')
     ingredients = models.ManyToManyField(Ingredient, through='Recipeingredients')
     description = models.CharField(max_length=20000)
+    tags = models.ManyToManyField(Tag)
     
     def __str__(self):
         return self.recipename
@@ -31,4 +49,29 @@ class Recipeingredients(models.Model):
     quantity = models.CharField(max_length=30)
     
     def __str__(self):
-        return self.ingredient + " is ingredient of " + self.recipe
+        return self.ingredient.name + " is ingredient of " + self.recipe.recipename
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    recipe = models.ForeignKey(Recipe)
+    author = models.ForeignKey(Person)
+    comment = models.CharField(max_length=2000)
+    
+    def __str__(self):
+        return "a comment for " + self.recipe.recipename
+    
+class Rating(models.Model):
+    id = models.AutoField(primary_key=True)
+    evaluator = models.ForeignKey(Person)
+    recipe = models.ForeignKey(Recipe)
+    RATING_CHOICES = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),              
+    )
+    rating = models.PositiveSmallIntegerField(choices=RATING_CHOICES, default=3)
+    
+    def __str__(self):
+        return "a rating for " + self.recipe.recipename
