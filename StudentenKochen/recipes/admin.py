c7dbf49142292f24f67936f1e8d2af2f10f5b846
KiwiBA <kiwi.bu@web.de>
2015-05-07 14:58:13 +0200

    Adminseite für leichtere benutzung

--- StudentenKochen/recipes/admin.py
+++ StudentenKochen/recipes/admin.py
@@ -7,17 +7,35 @@ from .models import Recipeingredients
 from .models import Comment
 from .models import Rating
 
-class IngredientsInline(admin.StackedInline):
+class IngredientsInline(admin.TabularInline):
     model = Recipeingredients
     extra = 3
 
 class RecipeAdmin(admin.ModelAdmin):
     fields = ['recipename', 'author', 'pub_date', 'description', 'tags']
     inlines = [IngredientsInline]
+    
+    list_display = ('id', 'recipename', 'author', 'pub_date')
+    list_filter = ['pub_date']
+    
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'author')
+    
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'evaluator', 'rating')
+    
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'pw')
+    
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tagname')
+    
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')    
 
-admin.site.register(Person)
-admin.site.register(Tag)
-admin.site.register(Ingredient)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Comment)
-admin.site.register(Rating)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Rating, RatingAdmin)
