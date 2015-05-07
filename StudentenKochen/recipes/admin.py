81dccd61c610144b7a0548c5450ff57a15c33418
KiwiBA <kiwi.bu@web.de>
2015-05-07 14:08:26 +0200

    Adminpage

--- StudentenKochen/recipes/admin.py
+++ StudentenKochen/recipes/admin.py
@@ -7,11 +7,17 @@ from .models import Recipeingredients
 from .models import Comment
 from .models import Rating
 
+class IngredientsInline(admin.StackedInline):
+    model = Recipeingredients
+    extra = 3
+
+class RecipeAdmin(admin.ModelAdmin):
+    fields = ['recipename', 'author', 'pub_date', 'description', 'tags']
+    inlines = [IngredientsInline]
 
 admin.site.register(Person)
 admin.site.register(Tag)
 admin.site.register(Ingredient)
-admin.site.register(Recipe)
-admin.site.register(Recipeingredients)
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Comment)
 admin.site.register(Rating)
