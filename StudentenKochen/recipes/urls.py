c28afcf27f03e7d69b38709c181960c88bc1b6f1
KiwiBA <kiwi.bu@web.de>
2015-05-26 14:18:54 +0200

    Rezepte bearbeiten

--- StudentenKochen/recipes/urls.py
+++ StudentenKochen/recipes/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^create/$', views.create, name='create'),
-    url(r'^edit/(?P<recipe_id>\d+)/$', views.create, name='edit'),
+    url(r'^edit/(?P<recipe_id>\d+)/$', views.edit, name='edit'),
 ]
