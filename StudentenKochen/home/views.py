79578bd00308eeb1ed5c6ded8d2712ba39abcd41
Sanim Rashid <sanim.rashid1@hs-augsburg.de@141.82.173.244>
2015-06-29 15:11:24 +0200

    code style angepasst in create, index und view  (home)

--- StudentenKochen/home/views.py
+++ StudentenKochen/home/views.py
@@ -1,7 +1,12 @@
+# -*- coding: utf8 -*-
 from django.shortcuts import render
 from recipes.models import Recipe
 from django.utils import timezone
 
 def index(request):
+    """
+    Lists the 20 up to date recipes ordered by pub_date.
+    """
     recipe_list = Recipe.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:20]
+    
     return render(request, 'home/index.html', {'recipe_list': recipe_list})
